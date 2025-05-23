f2f3812223d6c7daf4a02ebafa71f31682a3eec6
YujiKono(河野裕二) <yuji.kono.k083@dirbato.co.jp>
2025-05-23 17:06:11 +0900

    二回目コミット

---
+++ Sample/src/sample/Cast.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Cast {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        var a = 3; 
+        var b = 2; 
+        System.out.println( a / b ); 
+        System.out.println( (double)a / b ); 
+        System.out.println( (double)(a / b) ); 
+    }
+
+}
---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1; 
+        System.out.println(number1); 
+       
+        String str1; 
+        str1 = "abc"; 
+        System.out.println(str1);
+        
+        var x = 86400;
+        System.out.println(x);
+
+        int a = 123;
+        String s = Integer.toString(a);
+        System.out.println(s);
+    }
+
+}
